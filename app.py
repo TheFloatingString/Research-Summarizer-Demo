a4835b3342457dae3f4bd47ca66b051248c62c69
The Floating String <laurencel2001@gmail.com>
2021-06-18 14:40:14 -0400

    Create minimal version of summarizer for PubMed articles

---
+++ app.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+from summa import summarizer
+
+class SearchItem():
+
+	def __init__(self):
+		self.article_ids = list()
+		self.soup = BeautifulSoup("", "html.parser")
+		self.content_dict = dict()
+
+	def search_term(self, term):
+		count = 0
+		resp = requests.get(f"https://pubmed.ncbi.nlm.nih.gov/?term={term}")
+		self.soup = BeautifulSoup(resp.content, "html.parser")
+		for article_id in [x.get("data-article-id") for x in self.soup.find_all("a") if x.get("data-article-id") is not None]:
+			resp_article = requests.get(f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/")
+			sub_soup = BeautifulSoup(resp_article.content, "html.parser")
+			title = sub_soup.find(property="og:title")["content"].replace(" - PubMed", '')
+
+			abstract = sub_soup.find(id="abstract").get_text()
+
+			sub_dict = {}
+			sub_dict["id"] = article_id
+			sub_dict["title"] = title
+			sub_dict["abstract"] = abstract
+			sub_dict["summary"] = summarizer.summarize(abstract)
+			sub_dict["tags"] = [x.get("data-ga-label") for x in sub_soup.find_all(class_="keyword-actions-dropdown")]
+
+			self.content_dict[count] = sub_dict
+			count += 1
+
+		return self.content_dict
+
+
+obj = SearchItem()
+content = obj.search_term("cancer")
+print(content)
