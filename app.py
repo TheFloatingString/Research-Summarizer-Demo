04d85fd440bdaf903c991bcf81872db89c1adbea
The Floating String <laurencel2001@gmail.com>
2021-06-18 14:56:55 -0400

    Add REST API capability to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from summa import summarizer
+from flask import Flask
 
 import json
 
@@ -16,25 +17,38 @@ class SearchItem():
 		resp = requests.get(f"https://pubmed.ncbi.nlm.nih.gov/?term={term}")
 		self.soup = BeautifulSoup(resp.content, "html.parser")
 		for article_id in [x.get("data-article-id") for x in self.soup.find_all("a") if x.get("data-article-id") is not None]:
-			resp_article = requests.get(f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/")
-			sub_soup = BeautifulSoup(resp_article.content, "html.parser")
-			title = sub_soup.find(property="og:title")["content"].replace(" - PubMed", '')
+			try:
+				resp_article = requests.get(f"https://pubmed.ncbi.nlm.nih.gov/{article_id}/")
+				sub_soup = BeautifulSoup(resp_article.content, "html.parser")
+				title = sub_soup.find(property="og:title")["content"].replace(" - PubMed", '')
 
-			abstract = sub_soup.find(id="abstract").get_text()
+				abstract = sub_soup.find(id="abstract").get_text()
 
-			sub_dict = {}
-			sub_dict["id"] = article_id
-			sub_dict["title"] = title
-			sub_dict["abstract"] = abstract
-			sub_dict["summary"] = summarizer.summarize(abstract)
-			sub_dict["tags"] = [x.get("data-ga-label") for x in sub_soup.find_all(class_="search-in-pubmed-link")]
+				sub_dict = {}
+				sub_dict["id"] = article_id
+				sub_dict["title"] = title
+				sub_dict["abstract"] = abstract
+				sub_dict["summary"] = summarizer.summarize(abstract)
+				sub_dict["tags"] = [x.get("data-ga-label") for x in sub_soup.find_all(class_="search-in-pubmed-link")]
 
-			self.content_dict[count] = sub_dict
-			count += 1
+				self.content_dict[count] = sub_dict
+				count += 1
+			except:
+				pass
 
 		return self.content_dict
 
+app = Flask(__name__)
 
-obj = SearchItem()
-content = obj.search_term("cancer")
-print(json.dumps(content, indent=4, sort_keys=True))
+@app.route("/", methods=["GET"])
+def home():
+	return "Home"
+
+@app.route("/search/<search_name>", methods=["GET"])
+def search(search_name):
+	obj = SearchItem()
+	content = obj.search_term(search_name)
+	return json.dumps(content, indent=4, sort_keys=True)
+
+if __name__ == '__main__':
+	app.run(port=5000, host="0.0.0.0")
