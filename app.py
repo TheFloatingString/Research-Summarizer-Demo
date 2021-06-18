18039ba3ade11c539c3aa6d799e02083e526fa7b
The Floating String <laurencel2001@gmail.com>
2021-06-18 14:46:45 -0400

    Fix tags retrieval error

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from summa import summarizer
 
+import json
+
 class SearchItem():
 
 	def __init__(self):
@@ -25,7 +27,7 @@ class SearchItem():
 			sub_dict["title"] = title
 			sub_dict["abstract"] = abstract
 			sub_dict["summary"] = summarizer.summarize(abstract)
-			sub_dict["tags"] = [x.get("data-ga-label") for x in sub_soup.find_all(class_="keyword-actions-dropdown")]
+			sub_dict["tags"] = [x.get("data-ga-label") for x in sub_soup.find_all(class_="search-in-pubmed-link")]
 
 			self.content_dict[count] = sub_dict
 			count += 1
@@ -35,4 +37,4 @@ class SearchItem():
 
 obj = SearchItem()
 content = obj.search_term("cancer")
-print(content)
+print(json.dumps(content, indent=4, sort_keys=True))
